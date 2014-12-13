ecf94fbd319bedfc5921e8b10a0f46f66917962b
Thomas Richter <thomas.karl.richter@gmail.com>
2014-12-13 19:10:03 +0100

    hiding start button on other browser

--- meetingstimer.js
+++ meetingstimer.js
@@ -1,54 +1,82 @@
 Timer = new Mongo.Collection("timer");
 
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if(Timer.find().count()>0){
+      Timer.remove({_id: "123"});
+    }
+    Timer.insert({_id: "123", counter: 30});
+    // TODO set to some default
+    // code to run on server at startup
+  });
+}
+
+
 if (Meteor.isClient) {
   timeSpan = 30;
   isRuning = false;
-  Session.setDefault("counter", timeSpan);
+  Session.set("myRunning", false);
+  //Session.setDefault("counter", timeSpan);
 
   Template.uhr.helpers({
     counter: function () {
+      if(Timer.findOne("123").counter === 0){
+        //BUZZER
+        $(".clock")
+        .animate({"opacity": "toggle"}, "slow")
+        .animate({"opacity":"toggle","height": "toggle"}, "fast")
+        document.getElementById('buzzer').play();
+        //BUZZER
+      }
       return Timer.findOne("123").counter;
-      //return Session.get("counter");
+    },
+    isSelf: function () {
+      // wenn der timer nicht läuft
+      // der der es laufen lässt soll immer gezeigt werden zw. start und stopp
+      if (Meteor.user() && Timer.findOne("123").user && Timer.findOne("123").user._id !== Meteor.userId()) return false;
+      return true;
     }
   });
 
   Template.uhr.events({
     'change #time-value': function (event) {
-      Timer.update("123", {$set: {counter: +event.target.value}});
-      Session.set("counter", event.target.value);
+      timeSpan = +event.target.value;
+      Timer.update("123", {$set: {counter: timeSpan}});
       return false;
     },
     'click .start': function (event) {
-      Timer.update("123", {$inc: {counter: -1}});
-      Session.set("counter", Session.get("counter") - 1);
+      if(Timer.findOne("123").counter === 0) {
+        Timer.update("123", {$inc: {counter: timeSpan}, $set: {user: Meteor.user()}});
+      }
       if (!isRuning) {
         // RESET BUZZER
         $(".clock").css({"background-color":"#eee"});
         $(".start").html("Stopp");
         isRuning = setInterval(function(){
-          if(Session.get("counter") > 0)  {
-            Timer.update("123", {$inc: {counter: -1}});
-            //console.log(Timer.find("123").fetch());
-            Session.set("counter", Session.get("counter") - 1);
+          if(Timer.findOne("123").counter > 0)  {
+            if(Meteor.user()){
+              Session.set("myRunning", true);
+              Timer.update("123", {$inc: {counter: -1}, $set: {user: Meteor.user()}});
+              //console.log(Timer.find("123").fetch());
+              //Session.set("counter", Session.get("counter") - 1);
+            } else {
+              console.log("user not logged in");
+            }
           } else {
             console.log("BUZZER");
-            //BUZZER
-            $(".clock")
-              .animate({"opacity": "toggle"}, "slow")
-              .animate({"opacity":"toggle","height": "toggle"}, "fast")
-              .css("background-color", "red");
-            //BUZZER
+
             $(".start").html("Start");
-            Session.set("counter", this.$("#time-value")[0].value);
-            Timer.update("123", {$set: {counter: +this.$("#time-value")[0].value}});
+            Timer.update("123", {$set: {counter: timeSpan}, $set: {user: null}});
             clearInterval(isRuning);
+            if(Meteor.user()){
+              Session.set("myRunning", false);
+            }
             isRuning = false;
-            document.getElementById('buzzer').play();
           }
         }, 1000);
       } else {
         $(".start").html("Start");
-
+        Timer.update("123", {$set: {counter: timeSpan}, $set: {user: null}});
         clearInterval(isRuning);
         isRuning = false;
       }
@@ -64,19 +92,7 @@ if (Meteor.isClient) {
         clearInterval(isRuning);
         isRuning=false;
       }
-      Timer.update("123", {$set: {counter: +$("#time-value")[0].value}});
-      Session.set("counter", $("#time-value")[0].value);
+      Timer.update("123", {$set: {counter: timeSpan}, $set: {user: null}});
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if(Timer.find().count()>0){
-      Timer.remove({_id: "123"});
-    }
-    Timer.insert({_id: "123", counter: 30});
-    // TODO set to some default
-    // code to run on server at startup
   });
 }
